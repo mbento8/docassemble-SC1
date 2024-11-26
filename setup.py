8989fc0750b4a2e742303e80eb0ec5e58acff44b
Melissa Bento <146023939+mbento8@users.noreply.github.com>
2024-11-26 12:23:40 -0900

    Changed the version date, content is no different than last commit.

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ def find_package_data(where='.', package='', exclude=standard_exclude, exclude_d
     return out
 
 setup(name='docassemble.SC1',
-      version='11.20.24',
+      version='11.26.24',
       description=('A docassemble extension.'),
       long_description='# docassemble.SC1\r\n\r\nA docassemble extension.\r\n\r\n## Author\r\n\r\nMelissa Bento, MBento@akcourts.gov\r\n\r\n',
       long_description_content_type='text/markdown',
